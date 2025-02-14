f9c5d3e70e1233f54ca06434c55a154592f0c438
kardara <abdoulayekardara@gmail.com>
2025-02-14 10:07:56 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-bg': "url('/assets/avian-bird-flu.jpg')",
+      }
+    },
   },
   plugins: [],
 }
